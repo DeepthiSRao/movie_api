1671d7d421a7ecc71a89f78766c0e2b688e2797e
Deepthi Srinidhi Rao <deepthi2609@gmail.com>
2021-11-16 15:07:09 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+let http = require('http'),
+     url = require('url'),
+      fs = require('fs');
+
+http.createServer((request, response) =>{
+    let addr = request.url,
+           q = url.parse(addr, true),
+    filepath = '';           
+
+    fs.appendFile('log.txt', 'URL: ' + addr  + '\nTimestamp: ' + new Date() + '\n\n',
+        (err) =>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log('Data Added to log');
+            }
+        }
+    );
+
+    if(q.pathname.includes('documentation')){
+        filepath = (__dirname + '/documentation.html');
+    }else{
+        filepath = 'index.html';
+    }
+
+    fs.readFile(filepath, (err, data) =>{
+        if(err){
+            throw err;
+        }
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        response.write(data);
+        response.end('');
+    });
+}).listen(8080);
+
+console.log('My test server is running on Port 8080');
